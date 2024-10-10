815037afa066f41b6fe0e39855d915f7d236ab4a
BenjaminCline17 <duckmancline@gmail.com>
2024-10-10 15:54:30 -0500

    Completed practice-exam

--- index.js
+++ index.js
@@ -93,54 +93,99 @@ const collection = [
 
 // PROBLEM #1 //
 
-const setDigitalCode = function(){
-   
+const setDigitalCode = function(array, value){
+for (let i = array.length - 1; i >= 0; i -= 2){
+    array[i].digitalCode = value;
+} return array;  
 };
 
 // PROBLEM #2 //
 
-const filterByGenreTag = function(){
-    
+const filterByGenreTag = function(array, tag){
+   let genre = array.filter(function(x){
+    for (let i = 0; i < x.genreTags.length; i++){
+        if (x.genreTags[i] === tag){
+            return x;
+        }
+    }
+    });
+    return genre;
 };
 
 
 
 // PROBLEM #3 //
 
-const filterBySpecialFeatureType = function(){
-    
+const filterBySpecialFeatureType = function(array, type){
+    let result = array.filter(function(x) {
+        for (let i = 0; i < x.specialFeatures.length; i++){
+            if (x.specialFeatures[i]["type"] === type){
+                return x;
+            }
+        }
+    });
+    return result;
 };
 
 
 // PROBLEM #4 //
 
-const getTopSpecialFeature = function(array, title){
-    
+const getTopSpecialFeature = function(array, title, index=0){
+    //base
+    if (index >= array.length){
+        return "no matching title found";
+    }
+    if (array[index]["title"] === title){
+        return array[index].title + " Special Feature: " + array[index].specialFeatures[0]["title"]
+    }
+    //recursion
+    return getTopSpecialFeature(array, title, index + 1);
 };
 
 
 // PROBLEM #5 //
 
-const mapTitles = function(){
-    
+const mapTitles = function(array){
+   let result = array.map(function(x){
+    return x.title + " (" + x.year + ") " + "- " + "dir. " + x.director;
+   });
+   return result;
 };
 
 
 
 // PROBLEM #6 //
 
-const mapSpecialFeatures = function(){
-    
+const mapSpecialFeatures = function(array){
+   let result = array.map(function(x){
+    let output = [];
+    for (let i = 0; i < x.specialFeatures.length; i++){
+        output.push(x.specialFeatures[i]["title"]);
+    }
+    return output;
+   }); 
+   return result;
 };
 
 // PROBLEM #7 //
 
-const createNonsenseString = function(){
-    
+const createNonsenseString = function(array, index){
+let result = array.reduce(function(accumulator, current){
+    accumulator += current.title[index];
+    return accumulator;
+}, "")    
+    return result;
 };
 
 // PROBLEM #8 //
 
-const getValues = function(){
-    
+const getValues = function(object, props){
+let output = [];
+for (let i = 0; i < props.length; i++){
+    for (let keys in object){
+        if (keys == props[i]){
+            output.push(object[keys]);
+        }
+    }
+} return output;
 };
